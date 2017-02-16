a0bce4b9fde3636b4f900a9ddc51a687788ce557
vrathsmill <vlrathsmill@gmail.com>
2017-02-16 16:03:27 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,45 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    boats = self.all
+    boats.order("id ASC LIMIT 5")
+  end
+
+  def self.dinghy
+    boats = self.all
+    boats.where("length < 20")
+  end
+
+  def self.ship
+    boats = self.all
+    boats.where("length > 20")
+  end
+
+  def self.last_three_alphabetically
+    boats = self.all
+    boats.order("name DESC LIMIT 3")
+  end
+
+  def self.without_a_captain
+    boats = self.all
+    boats.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.includes(:classifications).where("classifications.name = ?", "Sailboat")
+  end
+
+
+
+  def self.with_three_classifications
+  Boat.joins(:classifications).group("Boats.id").having("Count(*)=3")
+end
+
+  def self.longest_boat
+    Boat.order("length DESC").limit(1)[0]
+end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+
+  def self.catamaran_operators
+    self.includes(:classifications).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+      self.includes(:classifications).where("classifications.name = ?", "Sailboat").group("Captains.name")
+  end
+
+  def self.talented_seamen
+    sailboat_captains = self.includes(:classifications).where("classifications.name = ?", "Sailboat")
+    motorboat_captains = self.includes(:classifications).where("classifications.name = ?", "Motorboat")
+    motorboat_captains.where({name: sailboat_captains.pluck(:name)})
+end
+
+  def self.non_sailors
+    sailor_names = self.includes(:classifications).where("classifications.name = ?", "Sailboat").pluck(:name)
+    Captain.where.not(name: sailor_names)
+  end
+
+
 end
